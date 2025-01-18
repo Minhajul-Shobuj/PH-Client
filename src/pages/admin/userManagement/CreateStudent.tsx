c5c8b4660999c28b271ce2c84e68c4562124f7a2
Jahid Hasan <mdminhajulislamshobuj@gmail.com>
2025-01-18 21:32:06 +0600

    update

--- src/pages/admin/userManagement/CreateStudent.tsx
+++ src/pages/admin/userManagement/CreateStudent.tsx
@@ -1,7 +1,52 @@
+import { Button, Col, Row } from "antd";
+import PHForm from "../../../component/form/PHForm";
+import PHInput from "../../../component/form/PHInput";
+import { FieldValues, SubmitHandler } from "react-hook-form";
+
 const CreateStudent = () => {
+  const onSubmit: SubmitHandler<FieldValues> = (data) => {
+    console.log(data);
+  };
   return (
     <>
-      <h1>Create Student</h1>
+      <Row>
+        <Col span={24}>
+          <PHForm onSubmit={onSubmit}>
+            <Row gutter={10}>
+              <Col span={24} md={{ span: 12 }} lg={{ span: 8 }}>
+                <PHInput
+                  type="text"
+                  name="name.firstName"
+                  label="First-name"
+                ></PHInput>
+              </Col>
+              <Col span={24} md={{ span: 12 }} lg={{ span: 8 }}>
+                <PHInput
+                  type="text"
+                  name="name.middleName"
+                  label="Middle-name"
+                ></PHInput>
+              </Col>
+              <Col span={24} md={{ span: 12 }} lg={{ span: 8 }}>
+                <PHInput
+                  type="text"
+                  name="name.lastName"
+                  label="Last-name"
+                ></PHInput>
+              </Col>
+              <Col span={24} md={{ span: 12 }} lg={{ span: 8 }}>
+                <PHInput
+                  type="text"
+                  name="lastName"
+                  label="Last-name"
+                ></PHInput>
+              </Col>
+            </Row>
+
+            <Button htmlType="submit">Submit</Button>
+          </PHForm>
+        </Col>
+      </Row>
     </>
   );
 };
